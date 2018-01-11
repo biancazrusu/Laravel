31c935859a2a0ccddade17c73365964834a39639
Bianca Rusu <bianca.rusu@icoldo.com>
2018-01-11 10:20:23 +0200

    my location add

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -8,7 +8,10 @@ class MapController extends Controller
 {
     public function index()
     {
-        Mapper::map(47.1585, 27.6014);
+        Mapper::map(47.1585, 27.6014)->informationWindow(47.1585, 27.6014, 'Content', ['markers' => ['animation' => 'DROP']]);
+        Mapper::location('Iasi, Copou')->streetview(1, 1, ['ui' => false]);
+        //my location
+        Mapper::map(0, 0, ['marker' => true, 'locate' => true, 'animation' => 'DROP','eventAfterLoad' => 'loadMarkerOverlay(map);','zoom' => 12 ]);
 
         return view('frontend.map');
     }
